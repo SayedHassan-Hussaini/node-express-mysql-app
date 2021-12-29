84af8fcc60e7c3eb3d2339576d0790ff9748ecd5
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2021-12-29 10:08:43 +0430

    work user login

--- models/Auth.js
+++ models/Auth.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
-const Joi =require('joi')
+const Joi = require('joi');
+const { response } = require('express');
+const { json } = require('express/lib/response');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -7,29 +9,71 @@ var connection = mysql.createConnection({
   database: 'node_db'
 })
 connection.connect()
-
-const validationUser =(data)=>{
-  const schema =Joi.object().keys({
-    firstname :Joi.string().min(3).max(250).required(),
-    lastname:Joi.string().min(3).max(250).required(),
-    email:Joi.string().email().required(),
-    password:Joi.string().min(6).max(300).lowercase(1)
-   })
-  const {error}=schema.validate(data)
-  if(error) return error.details
+//login
+const validationLogin = (data) => {
+  const schema = Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(300)
+  })
+  const { error } = schema.validate(data)
+  if (error) return error.details[0].message
   return "ok"
-} 
+}
+const login =async (data) => {
+  try{
+    const result =await new Promise((resolve, reject) => {
+      connection.query(`
+        SELECT id,firstname, lastname,email
+        FROM user   
+        WHERE email = '${data.email}' AND password='${data.password}'`, (err, rows, fields) => {
+        if (err){
+          console.log("err...",err)
+           reject(err)
+        }
+        else if (rows) {
+          if(rows.length ===0) resolve(data=[])
+          else{
+            const data=[
+              {
+                id:JSON.stringify(rows[0].id),
+                firstname:JSON.stringify(rows[0].firstname),
+                lastname:JSON.stringify(rows[0].lastname),
+                email:JSON.stringify(rows[0].email)
+              }
+            ]
+           resolve(data)
+          }  
+        }
+      });
+    })
+    return result
+  }catch(err){
+  return err.sqlMessage
+  }
+}
+// register
+const validationRegister = (data) => {
+  const schema = Joi.object().keys({
+    firstname: Joi.string().min(3).max(250).required(),
+    lastname: Joi.string().min(3).max(250).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(300).lowercase(1)
+  })
+  const { error } = schema.validate(data)
+  if (error) return error.details[0].message
+  return "ok"
+}
 const register = async (user) => {
   try {
-    
-  const result=  await new Promise((resolve, reject) => {
-      let result= connection.query(`INSERT INTO user VALUES('','${user.firstname}','${user.lastname}','${user.email}','${user.password}') `,
+
+    const result = await new Promise((resolve, reject) => {
+      let result = connection.query(`INSERT INTO user VALUES('','${user.firstname}','${user.lastname}','${user.email}','${user.password}') `,
         (err, res) => {
 
           if (err) reject(err)
           else {
             return resolve(result._results[0].insertId)
-          } 
+          }
         })
     })
     return result
@@ -37,5 +81,7 @@ const register = async (user) => {
     return err.sqlMessage;
   }
 }
-exports.validationUser=validationUser
+exports.login = login;
+exports.validationLogin = validationLogin
+exports.validationRegister = validationRegister
 exports.register = register
--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,73 @@
-const express =require('express')
-const config =require('config')
-const Jwt=require('jsonwebtoken')
-const router=express.Router()
-const {register,validationUser}=require('../models/Auth')
-
-router.post('/register',async(req,res)=>{
-    const isValidated=await validationUser(req.body)
-    if(isValidated !== "ok") res.status(400).send(isValidated)
-    const result=await register(req.body);
-    if(typeof result === "number"){ 
-        const token=Jwt.sign({id:result},config.get('jwtPrivateKey'))
-        const data=[
-            {   
-                status:200,
-                jwt:token,
-                id:result,
-                user:req.body
+const express = require('express')
+const config = require('config')
+const Jwt = require('jsonwebtoken')
+const router = express.Router()
+const { login, validationLogin, register, validationRegister } = require('../models/Auth')
+const { status } = require('express/lib/response')
+// Login
+router.post('/', async (req, res) => {
+    const validation = await validationLogin(req.body)
+    if (validation !== "ok") {
+        const error = [
+            {
+                status: 400,
+                message: validation
+            }
+        ]
+        res.status(400).send(error)
+    }
+    const result = await login(req.body)
+    if (result.length > 0) {
+        const token = Jwt.sign({ id: result[0].id }, config.get('jwtPrivateKey'))
+        const data = [
+            {
+                jwt: token,
+                status: 2000,
+                id:result[0].id,
+                user: result
+            }
+        ]
+        res.send(data)
+    } else {
+        const error = [
+            {
+                status: 400,
+                message: "Invalid email or password"
+            }
+        ]
+        res.status(400).send(error)
+    }
+})
+// Register
+router.post('/register', async (req, res) => {
+    const isValidated = await validationRegister(req.body)
+    if (isValidated !== "ok") {
+        const error = [
+            {
+                status: 400,
+                message: isValidated
+            }
+        ]
+        res.status(400).send(error)
+    }
+    const result = await register(req.body);
+    if (typeof result === "number") {
+        const token = Jwt.sign({ id: result }, config.get('jwtPrivateKey'))
+        const data = [
+            {
+                status: 200,
+                jwt: token,
+                id: result,
+                user: req.body
             }
         ]
         res.status(200).send(data)
     }
-    else{
-        const error=[
+    else {
+        const error = [
             {
-                status:400,
-                errorMessage:result
+                status: 400,
+                errorMessage: result
             }
         ]
         res.status(400).send(error)
@@ -32,4 +75,4 @@ router.post('/register',async(req,res)=>{
 })
 
 
-module.exports =router;
+module.exports = router;
